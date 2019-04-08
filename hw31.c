e02ffb7f3b5f21a4f7482f33505ddc43539c1e7d
Samuel Grenon <sgrenon@alaska.edu>
2019-04-07 19:44:26 -0800

    ...

--- hw31.c
+++ hw31.c
@@ -75,9 +75,6 @@ int main(int argc, char * argv[]){
 
         printf("The MPI value is: %d \n", min);
 
-        for(int i = 0; i <100; i++){
-            printf("%d ", a[i]);
-        }
     }
 
     free(a);
