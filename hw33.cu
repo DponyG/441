927007b887e9ff34a81db15ce540665fa97c64ad
Samuel Grenon <sgrenon@alaska.edu>
2019-04-09 14:55:12 -0800

    ..

---
+++ hw33.cu
@@ -0,0 +1,43 @@
+
+#include "stdio.h"
+#define COLUMNS 4
+#define ROWS 3
+
+__global__ void add(int * a, int*b)  {
+
+    int x = threadIdx.x;
+    int sum = 0;
+
+    for(unsigned int i = 0; i < ROWS; i++){
+        sum += a[i*COLUMNS+x];
+    }
+
+    b[x] = sum;
+    
+}
+
+int main() {
+    int a[ROWS][COLUMNS], n[ROWS][COLUMNS], c[ROWS][COLUMNS];
+    int *dev_a, *dev_b 
+
+    cudaMalloc((void **) &dev_a, ROWS*COLUMNS*sizeof(int));
+    cudaMalloc((void **) &dev_b, COLUMNS*sizeof(int));
+
+
+    for (int y = 0; y< ROWS; y++)
+        for(int x = 0; x < COLUMNS; x++){
+            a[y][x] = x;
+            b[y][x] = y;
+        }
+    
+    cudaMemcpy(dev_a, a, ROWS*COLUMNS*sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(dev_b, b, ROWS*COLUMNS*sizeof(int), cudaMemcpyHostToDevice);
+
+    add<<<1,COLUMNS>>>(dev_a, dev_b, dev_c);
+
+    cudaMemcpy(b, dev_b, COLUMNS*sizeof(int), cudaMemcpyDeviceToHost);
+
+    
+    
+
+}
