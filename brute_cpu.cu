4b1f26f606ecd125217f6bf3e66c5b75e6747fd7
Samuel Grenon <sgrenon@alaska.edu>
2019-04-26 18:05:25 -0800

    I hate this class

--- brute_cpu.cu
+++ brute_cpu.cu
@@ -11,103 +11,110 @@
  * if it matches the target hash.
  *
  ***********************************************************************/
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include "md5.cu"
-
-// Convert a decimal number (starting at 0) to a corresponding 6 letter string
-// using base 26 to represent the string
-// s must be big enough to hold 6 chars plus a null (7 chars total)
-void intToString(int num, char *s)
-{
-  int ones = (num) % 26;
-  int twentySix = (num / 26) % 26;
-  int twentySixSquared = (num / 26 / 26) % 26;
-  int twentySixCubed = (num / 26 / 26 / 26) % 26;
-  int twentySixFourth = (num / 26 / 26 / 26 / 26) % 26;
-  int twentySixFifth = (num / 26 / 26 / 26 / 26 / 26) % 26;
-  // Store appropriate char into the string
-  int i = 0;
-  s[i++] = twentySixFifth + 'A';
-  s[i++] = twentySixFourth + 'A';
-  s[i++] = twentySixCubed + 'A';
-  s[i++] = twentySixSquared + 'A';
-  s[i++] = twentySix + 'A';
-  s[i++] = ones + 'A';
-  s[i] = '\0';
-}
-
-// You may find this helpful for testing, this takes a 6 char string
-// like ABACAB and returns back the decimal number that maps to it
-// using the intToString function above
-int stringToInt(char *s)
-{
-  int length = strlen(s);
-  int sum = 0;
-  int power = 0;
-
-  for (int i = length-1; i >= 0; i--)
-  {
-	int digit = s[i] - 'A';
-	sum += digit * pow(26,power);	
-	power++;
-  } 
-  return sum;
-}
-
-
-// Brute force search over the space of numbers 0 - 26^6, mapped to all 6 char 
-// uppercase strings. The resulting string is hashed using md5 and compared
-// to the target hash to see if it is the same. If so, we just cracked the
-// original string that produced the md5 target.
-int main()
-{
-  // This is the md5 hash string we are trying to crack
-  char md5_hash_string[] = "070d912366b1cf46a01aaf93c99f907d";
-//  char md5_hash_string[] = "b381ce33225e82f0fd839d610c3832e5";
-  int md5Target[4];  // The md5 hash string extracted into four integers
-
-  // This loop extracts the md5 hash string into md5Target[0],[1],[2],[3]
-  for(int i = 0; i < 4; i++)
-  {
-    char tmp[16];
-    strncpy(tmp, md5_hash_string + i * 8, 8);
-    sscanf(tmp, "%x", &md5Target[i]);
-    md5Target[i] = (md5Target[i] & 0xFF000000) >> 24 | (md5Target[i] &
-                 0x00FF0000) >> 8 | (md5Target[i] & 0x0000FF00) << 8 |
-                (md5Target[i] & 0x000000FF) << 24;
-  }
-
-  // These variables are used to store the md5 hash for a string
-  // we generate in a brute force way to test if it matches the
-  // target
-  char possibleKey[7];  // Will be auto-generated AAAAAA to ZZZZZZ
-  uint32_t hashResult1, hashResult2, hashResult3, hashResult4;
-  uint8_t length = 6;
-
-  printf("Working on cracking the md5 key %s by trying all key combinations...\n",md5_hash_string);
-  // Assume we don't know the key, try brute force cracker by
-  // hashing all 6 letter strings from AAAAAA to ZZZZZZ
-  for (int i = 0; i < 26*26*26*26*26*26; i++)
-  {
-	intToString(i, possibleKey); 
-	md5Hash((unsigned char *) possibleKey, length, 
-		&hashResult1, &hashResult2, &hashResult3, &hashResult4);
-	if ((hashResult1 == md5Target[0]) &&
-            (hashResult2 == md5Target[1]) &&
-            (hashResult3 == md5Target[2]) &&
-            (hashResult4 == md5Target[3]))
-	{
-		printf("CRACKED! The original string is: %s\n", possibleKey);
-		break;
-	}	
-	// Comment out the below if you don't want to
-	// Occasionally print a message so we know it hasn't locked up
-	if (i % 250000 == 0)
-	{
-		printf("Guess #%d was %s\n", i, possibleKey);
-	}
- } 
-}
-
+ #include <stdio.h>
+ #include <string.h>
+ #include <math.h>
+ #include "md5.cu"
+ 
+ // Convert a decimal number (starting at 0) to a corresponding 6 letter string
+ // using base 26 to represent the string
+ // s must be big enough to hold 6 chars plus a null (7 chars total)
+ __device__ void intToString(int num, char *s)
+ {
+   int ones = (num) % 26;
+   int twentySix = (num / 26) % 26;
+   int twentySixSquared = (num / 26 / 26) % 26;
+   int twentySixCubed = (num / 26 / 26 / 26) % 26;
+   int twentySixFourth = (num / 26 / 26 / 26 / 26) % 26;
+   int twentySixFifth = (num / 26 / 26 / 26 / 26 / 26) % 26;
+   // Store appropriate char into the string
+   int i = 0;
+   s[i++] = twentySixFifth + 'A';
+   s[i++] = twentySixFourth + 'A';
+   s[i++] = twentySixCubed + 'A';
+   s[i++] = twentySixSquared + 'A';
+   s[i++] = twentySix + 'A';
+   s[i++] = ones + 'A';
+   s[i] = '\0';
+ }
+ 
+ // You may find this helpful for testing, this takes a 6 char string
+ // like ABACAB and returns back the decimal number that maps to it
+ // using the intToString function above
+ int stringToInt(char *s)
+ {
+   int length = strlen(s);
+   int sum = 0;
+   int power = 0;
+ 
+   for (int i = length-1; i >= 0; i--)
+   {
+   int digit = s[i] - 'A';
+   sum += digit * pow(26,power);	
+   power++;
+   } 
+   return sum;
+ };
+ 
+ __global__ void crack(char* result, int* md5Target){
+   result[0] = 'A'
+   uint32_t *hashResult1, *hashResult2, *hashResult3, *hashResult4;
+   char possibleKey[7];
+   int length = 6;
+   for (int i = 0; i < 26*26*26*26*26*26; i++){
+     intToString(i, possibleKey); 
+     md5Hash((unsigned char*) possibleKey, length, hashResult1, hashResult2, hashResult3, hashResult4);
+     if ((*hashResult1 == md5Target[0]) &&
+         (*hashResult2 == md5Target[1]) &&
+         (*hashResult3 == md5Target[2]) &&
+         (*hashResult4 == md5Target[3]))
+     {
+       (*result) = (*possibleKey);
+       return;
+     }
+   }
+ };
+ // Brute force search over the space of numbers 0 - 26^6, mapped to all 6 char 
+ // uppercase strings. The resulting string is hashed using md5 and compared
+ // to the target hash to see if it is the same. If so, we just cracked the
+ // original string that produced the md5 target.
+ int main()
+ {
+   // This is the md5 hash string we are trying to crack
+ //  char md5_hash_string[] = "070d912366b1cf46a01aaf93c99f907d";
+   char md5_hash_string[] = "b381ce33225e82f0fd839d610c3832e5";
+   int md5Target[4];  // The md5 hash string extracted into four integers
+ 
+   // This loop extracts the md5 hash string into md5Target[0],[1],[2],[3]
+   for(int i = 0; i < 4; i++)
+   {
+     char tmp[16];
+     strncpy(tmp, md5_hash_string + i * 8, 8);
+     sscanf(tmp, "%x", &md5Target[i]);
+     md5Target[i] = (md5Target[i] & 0xFF000000) >> 24 | (md5Target[i] &
+                  0x00FF0000) >> 8 | (md5Target[i] & 0x0000FF00) << 8 |
+                 (md5Target[i] & 0x000000FF) << 24;
+   }
+ 
+   // These variables are used to store the md5 hash for a string
+   // we generate in a brute force way to test if it matches the
+   // target
+   char dev_result[7];
+   int dev_md5Target[4];
+   char *result = (char *) malloc(sizeof(char)*7);  // Will be auto-generated AAAAAA to ZZZZZZ
+   cudaMalloc((void **) &dev_md5Target, 4*sizeof(int));
+   cudaMalloc((void **) &dev_result, 7*sizeof(char));
+   cudaMemcpy(dev_md5Target, md5Target, 4*sizeof(int),cudaMemcpyHostToDevice);
+   cudaMemcpy(dev_result,result,7*sizeof(char),cudaMemcpyHostToDevice);
+     
+   crack<<<26,26>>>(dev_result, dev_md5Target);
+   printf("Error: %s \n",cudaGetErrorName(cudaGetLastError()));
+   printf("ErrorDes: %s \n",cudaGetErrorString(cudaGetLastError()));
+   printf("Working on cracking the md5 key %s by trying all key combinations...\n",md5_hash_string);
+   cudaMemcpy(result,dev_result, 7*sizeof(char),cudaMemcpyDeviceToHost);
+   printf("hopefully: %s \n",result[0]);
+   cudaFree(dev_md5Target);
+   cudaFree(dev_result);
+   // Assume we don't know the key, try brute force cracker by
+   // hashing all 6 letter strings from AAAAAA to ZZZZZZ
+ }
--- sobel-cpu.cu
+++ sobel-cpu.cu
@@ -12,111 +12,111 @@
  * Compile/run with:  nvcc sobel-cpu.cu -lfreeimage
  *
  ***********************************************************************/
- #include "FreeImage.h"
- #include "stdio.h"
- #include "math.h"
- 
- 
- #define threadsPerBlock 16
- // Returns the index into the 1d pixel array
- // Given te desired x,y, and image width
- __device__ int pixelIndex(int x, int y, int width)
- {
-     return (y*width + x);
- }
- 
- // Returns the sobel value for pixel x,y
- __global__ void sobel(char *pixelsOut, int width, int height, char *pixels)
- {
-     int x = threadIdx.x + blockIdx.x * blockDim.x;
-     int y = threadIdx.y + blockIdx.y * blockDim.y;
-     
-         int x00 = -1;  int x20 = 1;
-         int x01 = -2;  int x21 = 2;
-         int x02 = -1;  int x22 = 1;
-         x00 *= pixels[pixelIndex(x-1,y-1,width)];
-         x01 *= pixels[pixelIndex(x-1,y,width)];
-         x02 *= pixels[pixelIndex(x-1,y+1,width)];
-         x20 *= pixels[pixelIndex(x+1,y-1,width)];
-         x21 *= pixels[pixelIndex(x+1,y,width)];
-         x22 *= pixels[pixelIndex(x+1,y+1,width)];
-         
-         int y00 = -1;  int y10 = -2;  int y20 = -1;
-         int y02 = 1;  int y12 = 2;  int y22 = 1;
-         y00 *= pixels[pixelIndex(x-1,y-1,width)];
-         y10 *= pixels[pixelIndex(x,y-1,width)];
-         y20 *= pixels[pixelIndex(x+1,y-1,width)];
-         y02 *= pixels[pixelIndex(x-1,y+1,width)];
-         y12 *= pixels[pixelIndex(x,y+1,width)];
-         y22 *= pixels[pixelIndex(x+1,y+1,width)];
- 
-         int px = x00 + x01 + x02 + x20 + x21 + x22;
-         int py = y00 + y10 + y20 + y02 + y12 + y22;
-         pixelsOut[pixelIndex(x,y,width)] = sqrt(float(px*px + py*py));
-     
- }
- 
- int main()
- {
-     FreeImage_Initialise();
-     atexit(FreeImage_DeInitialise);
- 
-     // Load image and get the width and height
-     FIBITMAP *image;
-     image = FreeImage_Load(FIF_PNG, "coins.png", 0);
-     if (image == NULL)
+#include "FreeImage.h"
+#include "stdio.h"
+#include "math.h"
+
+
+#define threadsPerBlock 16
+// Returns the index into the 1d pixel array
+// Given te desired x,y, and image width
+__device__ int pixelIndex(int x, int y, int width)
+{
+    return (y*width + x);
+}
+
+// Returns the sobel value for pixel x,y
+__global__ void sobel(char *pixelsOut, int width, int height, char *pixels)
+{
+    int x = threadIdx.x + blockIdx.x * blockDim.x;
+    int y = threadIdx.y + blockIdx.y * blockDim.y;
+    
+        int x00 = -1;  int x20 = 1;
+        int x01 = -2;  int x21 = 2;
+        int x02 = -1;  int x22 = 1;
+        x00 *= pixels[pixelIndex(x-1,y-1,width)];
+        x01 *= pixels[pixelIndex(x-1,y,width)];
+        x02 *= pixels[pixelIndex(x-1,y+1,width)];
+        x20 *= pixels[pixelIndex(x+1,y-1,width)];
+        x21 *= pixels[pixelIndex(x+1,y,width)];
+        x22 *= pixels[pixelIndex(x+1,y+1,width)];
+        
+        int y00 = -1;  int y10 = -2;  int y20 = -1;
+        int y02 = 1;  int y12 = 2;  int y22 = 1;
+        y00 *= pixels[pixelIndex(x-1,y-1,width)];
+        y10 *= pixels[pixelIndex(x,y-1,width)];
+        y20 *= pixels[pixelIndex(x+1,y-1,width)];
+        y02 *= pixels[pixelIndex(x-1,y+1,width)];
+        y12 *= pixels[pixelIndex(x,y+1,width)];
+        y22 *= pixels[pixelIndex(x+1,y+1,width)];
+
+        int px = x00 + x01 + x02 + x20 + x21 + x22;
+        int py = y00 + y10 + y20 + y02 + y12 + y22;
+        pixelsOut[pixelIndex(x,y,width)] = sqrt(float(px*px + py*py));
+
+}
+
+int main()
+{
+    FreeImage_Initialise();
+    atexit(FreeImage_DeInitialise);
+
+    // Load image and get the width and height
+    FIBITMAP *image;
+    image = FreeImage_Load(FIF_PNG, "coins.png", 0);
+    if (image == NULL)
+    {
+        printf("Image Load Problem\n");
+        exit(0);
+    }
+    int imgWidth;
+    int imgHeight;
+    imgWidth = FreeImage_GetWidth(image);
+    imgHeight = FreeImage_GetHeight(image);
+
+    // Convert image into a flat array of chars with the value 0-255 of the
+    // greyscale intensity
+    RGBQUAD aPixel;
+    char *pixels;  
+    int pixIndex = 0;
+    pixels = (char *) malloc(sizeof(char)*imgWidth*imgHeight);
+    for (int i = 0; i < imgHeight; i++)
+     for (int j = 0; j < imgWidth; j++)
      {
-         printf("Image Load Problem\n");
-         exit(0);
+       FreeImage_GetPixelColor(image,j,i,&aPixel);
+       char grey = ((aPixel.rgbRed + aPixel.rgbGreen + aPixel.rgbBlue)/3);
+       pixels[pixIndex++]=grey;
      }
-     int imgWidth;
-     int imgHeight;
-     imgWidth = FreeImage_GetWidth(image);
-     imgHeight = FreeImage_GetHeight(image);
- 
-     // Convert image into a flat array of chars with the value 0-255 of the
-     // greyscale intensity
-     RGBQUAD aPixel;
-     char *pixels;  
-     int pixIndex = 0;
-     pixels = (char *) malloc(sizeof(char)*imgWidth*imgHeight);
-     for (int i = 0; i < imgHeight; i++)
-      for (int j = 0; j < imgWidth; j++)
+
+    dim3 numThreads(threadsPerBlock, threadsPerBlock, 1);
+    dim3 numBlocks(ceil(imgWidth/threadsPerBlock), ceil(imgHeight/threadsPerBlock), 1);
+    // Apply sobel operator to pixels, ignoring the borders
+    FIBITMAP *bitmap = FreeImage_Allocate(imgWidth, imgHeight, 24);
+    char *dev_pixels;
+    char *dev_pixelReturn;
+    char *computedPixels = (char *) malloc(sizeof(char)*imgWidth*imgHeight);
+    cudaMalloc((void**) &dev_pixels, sizeof(char)*imgWidth*imgHeight);
+    cudaMalloc((void**) &dev_pixelReturn, sizeof(char)*imgWidth*imgHeight);
+    cudaMemcpy(dev_pixels, pixels, sizeof(char)*imgWidth*imgHeight, cudaMemcpyHostToDevice);
+    cudaMemcpy(dev_pixelReturn, computedPixels, sizeof(char)*imgWidth*imgHeight, cudaMemcpyHostToDevice);
+    sobel<<<numBlocks, numThreads>>>(dev_pixelReturn, imgWidth, imgHeight, dev_pixels);
+    cudaMemcpy(computedPixels, dev_pixelReturn, sizeof(char)*imgWidth*imgHeight, cudaMemcpyDeviceToHost);
+    for (int i = 1; i < imgWidth-1; i++)
+    {
+      for (int j = 1; j < imgHeight-1; j++)
       {
-        FreeImage_GetPixelColor(image,j,i,&aPixel);
-        char grey = ((aPixel.rgbRed + aPixel.rgbGreen + aPixel.rgbBlue)/3);
-        pixels[pixIndex++]=grey;
+    // int sVal = sobel(i,j,imgWidth,pixels);
+        int sVal = float(computedPixels[j * imgWidth + i]);
+        aPixel.rgbRed = sVal;
+        aPixel.rgbGreen = sVal;
+        aPixel.rgbBlue = sVal;
+        FreeImage_SetPixelColor(bitmap, i, j, &aPixel);
       }
- 
-     dim3 numThreads(threadsPerBlock, threadsPerBlock, 1);
-     dim3 numBlocks(ceil(imgWidth/threadsPerBlock), ceil(imgHeight/threadsPerBlock), 1);
-     // Apply sobel operator to pixels, ignoring the borders
-     FIBITMAP *bitmap = FreeImage_Allocate(imgWidth, imgHeight, 24);
-     char *dev_pixels;
-     char *dev_pixelReturn;
-     char *computedPixels = (char *) malloc(sizeof(char)*imgWidth*imgHeight);
-     cudaMalloc((void**) &dev_pixels, sizeof(char)*imgWidth*imgHeight);
-     cudaMalloc((void**) &dev_pixelReturn, sizeof(char)*imgWidth*imgHeight);
-     cudaMemcpy(dev_pixels, pixels, sizeof(char)*imgWidth*imgHeight, cudaMemcpyHostToDevice);
-     cudaMemcpy(dev_pixelReturn, computedPixels, sizeof(char)*imgWidth*imgHeight, cudaMemcpyHostToDevice);
-     sobel<<<numBlocks, numThreads>>>(dev_pixelReturn, imgWidth, imgHeight, dev_pixels);
-     cudaMemcpy(computedPixels, dev_pixelReturn, sizeof(char)*imgWidth*imgHeight, cudaMemcpyDeviceToHost);
-     for (int i = 1; i < imgWidth-1; i++)
-     {
-       for (int j = 1; j < imgHeight-1; j++)
-       {
-     // int sVal = sobel(i,j,imgWidth,pixels);
-         int sVal = computedPixels[j * imgWidth + i];
-         aPixel.rgbRed = sVal;
-         aPixel.rgbGreen = sVal;
-         aPixel.rgbBlue = sVal;
-         FreeImage_SetPixelColor(bitmap, i, j, &aPixel);
-       }
-     }
-     FreeImage_Save(FIF_PNG, bitmap, "coins-edge.png", 0);
-   
-     free(pixels);
-     FreeImage_Unload(bitmap);
-     FreeImage_Unload(image);
-     return 0;
- }
+    }
+    FreeImage_Save(FIF_PNG, bitmap, "coins-edge.png", 0);
+  
+    free(pixels);
+    FreeImage_Unload(bitmap);
+    FreeImage_Unload(image);
+    return 0;
+}
