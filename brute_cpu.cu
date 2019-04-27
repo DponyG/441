192e8838d40c31b64da05f0b0e74fa992407798d
Samuel Grenon <sgrenon@alaska.edu>
2019-04-26 19:00:01 -0800

    ..

--- brute_cpu.cu
+++ brute_cpu.cu
@@ -71,9 +71,9 @@
          (hashResult4 == md5Target[3]))
      {
       printf("Hello from block %d, This thread made it %d, The possibleKeys is %d \n", blockIdx.x, threadIdx.x, *possibleKey);
-       *result = *possibleKey;
+      result = possibleKey;
        
-       asm("trap;");
+      asm("trap;");
        return;
      }
    }
@@ -116,7 +116,7 @@
    printf("ErrorDes: %s \n",cudaGetErrorString(cudaGetLastError()));
    printf("Working on cracking the md5 key %s by trying all key combinations...\n",md5_hash_string);
    cudaMemcpy(result,dev_result, 7*sizeof(char),cudaMemcpyDeviceToHost);
-   printf("hopefully: %s \n",result);
+   printf("hopefully: %s \n",*result);
    cudaFree(dev_md5Target);
    cudaFree(dev_result);
    // Assume we don't know the key, try brute force cracker by
